eddcbe213f04d20015e34a6739488a7f1675d02b
Iain Goh <iaingoh@gmail.com>
2019-01-12 17:12:59 +0800

    Third commit: moved the resource folder into the cards app directory; created the view that loads the json response; tested the view the url route; url route is called /dev/ for now; the json loads, but im having trouble deciding how i should load the dictionary typed card into a model class in Django which i've not done before; ok now, i realize that i should pull out the thirty cards before loading into the model class; if not to load into the model class and go directly to the API call, it might be possible too.

--- backendTest/cards/views.py
+++ backendTest/cards/views.py
@@ -1,3 +1,24 @@
 from django.shortcuts import render
+from django.http import HttpResponse
+import json
 
-# Create your views here.
+def load_json(request):
+    with open('cards/resources/response.json') as f:
+        collection = json.load(f)
+
+        for card in collection:
+            print(card['name'])
+
+        print(len(collection))
+        print(type(collection))
+
+        card1 = collection[0]
+        print(type(card1))
+        print(len(card1))
+
+        card2 = collection[1]
+        print(len(card2))
+
+
+    return HttpResponse('<h3>The view is loaded</h3>')
+
