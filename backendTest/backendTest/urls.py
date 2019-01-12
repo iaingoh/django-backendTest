d11eb60a16ac783b1be374936d6e14a5090ed5c3
Iain Goh <iaingoh@gmail.com>
2019-01-12 17:59:56 +0800

    Fifth commit: I realized that each card's attributes might be different in a small or large way than other cards and so save to a defined model class was not an option with my current level of understanding; I chose to bypass saving to a model class and exposed it straight to the endpoint: '/cards/' ; Django REST framework displays the thirty card deck; the deck may not be of the same playerClass and cards may repeat; next up will deploy to heroku and see if it still works

--- backendTest/backendTest/urls.py
+++ backendTest/backendTest/urls.py
@@ -1,7 +1,9 @@
 from django.contrib import admin
 from django.urls import path, include
+from cards import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('dev/', include('cards.urls'))
+    path('dev/', include('cards.urls')),
+    path('cards/', views.CardList.as_view())
 ]
--- backendTest/cards/views.py
+++ backendTest/cards/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.http import HttpResponse
+from rest_framework.views import APIView
+from rest_framework.response import Response
 import json
 import random
 
@@ -16,6 +18,23 @@ def load_json(request):
 
     print(len(deck))
     return HttpResponse('<h3>The view is loaded</h3>')
+
+class CardList(APIView):
+
+    def get(self, request):
+        with open('cards/resources/response.json') as f:
+            collection = json.load(f)
+            deck = []
+
+            for i in range(30):
+                deck.append(random.choice(collection))
+
+            for card in deck:
+                print(card['name'])
+
+        print(len(deck))
+        return Response(deck)
+
 
 
 
