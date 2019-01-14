5bf37c1709d6c71c8a8674b91eb5ad1b8ea83c17
Iain Goh <iaingoh@gmail.com>
2019-01-14 21:56:00 +0800

    Eigth commit: key error fixed by implementing faulty card removal algorithm; there were only two faulty cards in the original collection; trimmed off the flag and implemented the deck length checking directly into the while loop; the entire while loop is much leaner, contains fewer lines of code, meets project objectives; next: implement a repeat card check to prevent cards from repeating more than twice.

--- backendTest/cards/views.py
+++ backendTest/cards/views.py
@@ -24,24 +24,33 @@ class CardList(APIView):
     def get(self, request):
         with open('cards/resources/response.json') as f:
             collection = json.load(f)
+            print("There are " + str(len(collection)) + " cards in the collection.")
 
-            deck = []
-            flag = True
-            while flag:
-                card = random.choice(collection)
-                if not deck:
+        # Remove cards where key playerClass does not exist.
+        faulty_cards = []
+        for card in collection:
+            if 'playerClass' not in card:
+                faulty_cards.append(card)
+                index = collection.index(card)
+                del collection[index]
+        print("The conditional found " + str(len(faulty_cards)) + " faulty cards.")
+        print("There are now " + str(len(collection)) + " cards in the collection.")
+
+        deck = []
+        while len(deck) < 30:
+            card = random.choice(collection)
+            if not deck:
+                deck.append(card)
+                playerClass = card['playerClass']
+                continue
+            elif card['playerClass'] == playerClass or card['playerClass'] == 'Neutral':
                     deck.append(card)
-                    playerClass = card['playerClass']
-                    continue
-                elif len(deck) < 30:
-                    if card['playerClass'] == playerClass or card['playerClass'] == 'Neutral':
-                        deck.append(card)
-                else:
-                    flag = False
-            for card in deck:
-                print(card['playerClass'])
-            print(len(deck))
-        
+
+        for card in deck:
+            print(card['playerClass'])
+
+        print(len(deck))
+    
         return Response(deck)
 
 
